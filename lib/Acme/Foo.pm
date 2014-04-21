e77febe31e2751f7654a762c466c84d42e1aaa5f
Alex <alexbyk@cpan.org>
2014-04-21 16:55:31 +0000

    Import of ALEXBYK/Acme-Foo-0.00_01 from CPAN.
    gitpan-cpan-distribution: Acme-Foo
    gitpan-cpan-version:      0.00_01
    gitpan-cpan-path:         ALEXBYK/Acme-Foo-0.00_01.tar.gz
    gitpan-cpan-author:       ALEXBYK
    gitpan-cpan-maturity:     developer

---
+++ lib/Acme/Foo.pm
@@ -0,0 +1,51 @@
+package Acme::Foo;
+use strict;
+use warnings;
+
+our $VERSION = '0.00_01';    # VERSION
+sub foo { return 1; }
+
+1;
+
+# ABSTRACT: brand new module Acme::Foo
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Acme::Foo - brand new module Acme::Foo
+
+=head1 VERSION
+
+version 0.00_01
+
+=head1 SYNOPSIS
+
+  ...
+
+=head1 DESCRIPTION
+
+Some description here.
+
+=head1 METHODS
+
+=head2 foo
+
+This method does something amazing.
+
+=head1 AUTHOR
+
+alexbyk <alex@alexbyk.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by alexbyk.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
---
+++ t/0.t
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+
+use Test::More;
+ok 1, "initial test";
+
+done_testing;
